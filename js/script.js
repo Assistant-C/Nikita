845d422e34963f8b4a52675af504665660510d4d
Assistant-C <50529706+Assistant-C@users.noreply.github.com>
2024-06-21 22:01:54 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+function project_fun(){
+    $(".project_info").show()
+    $(".info_phone").hide()
+    $(".me_info").hide()
+}
+function info_fun(){
+    $(".project_info").hide()
+    $(".info_phone").show()
+    $(".me_info").hide()
+}
+function me_fun(){
+    $(".project_info").hide()
+    $(".info_phone").hide()
+    $(".me_info").show()
+}
+function home_fun(){
+    $(".project_info").hide()
+    $(".info_phone").hide()
+    $(".me_info").hide()
+}
+// $( "div.project_info" ).scrollTop( 500 );
+// $( "div.info_phone").scrollTop( 500 );
+// $( "div.me_info").scrollTop( 500 );
